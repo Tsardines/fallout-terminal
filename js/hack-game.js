6733804f25623860a9ab00e1cff7f73ca49f618b
Haley Sulcer <hjsulcer@gmail.com>
2018-02-28 21:37:34 -0500

    created html boilerplate and arrays

---
+++ js/hack-game.js
@@ -0,0 +1,33 @@
+var garbage = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", ";", "<", ">", "/", "?", "~"];
+
+var words = ["REAPER", "TRYING", "VERMIN", "MISERY", "CRISPY", "TRUANT", "ARGUES", "THINKS", "KILLER", "LESSON", "JUMPER", "HOLLOW", "WILLOW", "MOTHER", "FATHER", "WRENCH", "NIMBUS", "RABIES", "TOASTY", "PULLER", "VULGAR", "BENDER", "KITTEN", "NAPPER", "DAPPER"];
+// why the color change?
+
+var rowStart = ["0xN0H1", "0xN0H2", "0xN0H3", "0xN0H4", "0xN0H5", "0xN0H6", "0xN0H7", "0xN0H8", "0xN0H9", "0xN0H0", "1xN0H1", "1xN0H2", "1xN0H3", "1xN0H4", "1xN0H5", "1xN0H6", "1xN0H7", "1xN0H8", "1xN0H9", "1xN0H0"]
+
+var newArray = [];
+
+///// ======== ////// ATTEMPTS ///// ======== //////
+
+
+
+
+///// ======== ////// ROW START ///// ======== //////
+
+
+
+
+
+///// ======== ////// RANDOM WORDS ///// ======== //////
+
+
+
+
+
+///// ======== ////// GARBAGE CHARACTERS ///// ======== //////
+
+
+
+
+
+///// ======== ////// PLAYER SELECT & COMPARE ///// ======== //////
