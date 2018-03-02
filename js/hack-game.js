d1ddd98400d8ea5f9a9e09f172750eb6e423a420
Haley Sulcer <hjsulcer@gmail.com>
2018-03-02 14:32:18 -0500

    sized giantArray appropriately

--- js/hack-game.js
+++ js/hack-game.js
@@ -2,11 +2,15 @@ var garbage = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", 
 ",", "."];
 
 var words = ["REAPER", "TRYING", "VERMIN", "MISERY", "CRISPY", "TRUANT", "ARGUES", "THINKS", "KILLER", "LESSON", "JUMPER", "HOLLOW", "WILLOW", "MOTHER", "FATHER", "WRENCH", "NIMBUS", "RABIES", "TOASTY",
-  "PULLER", "TIMBER", "VULGAR", "BENDER", "KITTEN", "NAPPER", "DAPPER", "CHANGER", "BEAKER", "APPLES", "ORANGE", "BUILDER", "FALCON", "LOGGER", "ABROAD", "ACCEPT", "ACTION", "MERGER", "MIDDLE", "MEMORY",
-  "SALARY", "SCREEN", "SCHEME", "ROBUST", "SYMBOL", "TALENT", "WINDOW", "WORKER", "WRITER", "YELLOW", "GREENS", "TENDER", "TENNIS", "RECORD", "REMOVE", "REMAIN", "NEARLY", "NOBODY", "NOTICE", "NOTION",
+  "PULLER", "TIMBER", "VULGAR", "BENDER", "KITTEN", "NAPPER", "DAPPER", "CHANGE", "BEAKER", "APPLES", "ORANGE", "BUILDS", "FALCON", "LOGGER", "IDIOTS", "ABROAD", "ACCEPT", "ACTION", "MERGER", "MIDDLE",
+  "MEMORY", "SALARY", "SCREEN", "SCHEME", "ROBUST", "SYMBOL", "TALENT", "WINDOW", "WIDOWS", "WORKER", "WRITER", "YELLOW", "GREENS", "TENDER", "TENNIS", "RECORD", "REMOVE", "REMAIN", "NEARLY", "NOBODY",
+  "NOTICE", "NOTION", "STYLES", "TRICKY", "HAVING", "DANCER", "LAMBDA", "KOSHER", "KNEELS", "KNOBBY", "LAUGHS", "LEGION", "HUMANS", "LEMONS", "LYCHEE", "COOKIE", "PROVEN", "POTTER", "QUOHOQ", "QUIRKY",
+  "PURPLE", "QUAINT", "QWERTY", "PREFIX", "SHOJIS", "SEESAW", "SEAMAN", "SUBWAY", "SICKLE", "SIGNET", "SIRENS", "SEIZED", "SINEWY", "VIKING", "WAFFLE", "VELOUR", "VIABLE", "WHINGE", "VIOLET", "WIDELY",
+  "VELVET", "WEAKEN", "WIZARD", "WINERY", "ZEALOT", "MUDCAP", "MUSING", "MUSICS", "NEATEN", "NOISES", "NOGGIN", "NOMADS", "MOSQUE", "MONGOL", "HOLLER", "ILLEST", "ICONIC", "IMPISH", "INBRED", "INFLUX",
+  "INTAKE", "INHALE", "IMPOSE", "CROWNS", "CRUMBS", "CREEPS", "CRADLE", "CRACKS", "CRAMPY",
   "NORMAL", "MURDER", "MARINE", "FAMOUS", "EXTENT", "FACING", "FINISH", "FABRIC", "CASTLE", "CENTER", "CARBON", "FRIEND", "GARDEN", "GOLDEN", "GERMAN", "POLISH", "IMPACT", "IMPORT", "HEALTH", "REDUCE",
   "REASON", "REGIME", "RELIEF", "READER", "REMOTE", "SECRET", "SECTOR", "SECOND", "SCHOOL", "SURVEY", "THEORY", "TWELVE", "WINTER", "SUMMER", "AUTUMN", "WONDER", "PRETTY", "PROVEN", "PRINCE", "PUBLIC",
-  "PLANET", "PLENTY", "PLAYER", "BEAUTY", "BISHOP", "BOTTLE", "BREATH", "BRANCH", "CAMERA", "BUTTON", "BELONG", "BETTER"
+  "PLANET", "PLENTY", "PLAYER", "BEAUTY", "BISHOP", "BOTTLE", "BREATH", "BRANCH", "CAMERA", "BUTTON", "BELONG", "BETTER", "CANOPY", "CARAFE", "GLIDER", "YEOMAN", "JAGGED", "ORGANS", "VERMIN", "CYPHER"
 ];
 
 // var zeroOneCode = ["0xN0H1", "0xN0H2", "0xN0H3", "0xN0H4", "0xN0H5", "0xN0H6", "0xN0H7", "0xN0H8", "0xN0H9", "0xN0H0", "1xN0H1", "1xN0H2", "1xN0H3", "1xN0H4", "1xN0H5", "1xN0H6", "1xN0H7", "1xN0H8", "1xN0H9",
@@ -25,7 +29,15 @@ var discardedWords = []; // incorrect words that the player previously chose
 
 var playerAttempts = []; // how many past attempts the user has made
 
+
 ///// ======== ////// LANDING PAGES 1 AND 2 ///// ======== //////
+
+var form = document.querySelector('.form')
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault(); // will prevent the refresh from happening---form won't submit to another page
+  var nameInputDOMElem = document.querySelector('.name');
+  console.log(nameInputDOMElem.value);
+});
 
 // click on power dial
 // Landing 2 appears
@@ -37,7 +49,6 @@ var playerAttempts = []; // how many past attempts the user has made
 ///// ======== ////// ATTEMPTS ///// ======== //////
 
 // this shows how many attempts the player has left
-// this is updated after incorrect userWords get pushed into the discardedWords array
 
 for (var i = 0; i < 1; i++) {
   if (playerAttempts.length === 4) {
@@ -73,45 +84,18 @@ for (var i = 0; i < 1; i++) {
 
 // create a single DOM node
 // fill it with the text with the first word from the words array
-//
+
 let createAndDisplayWordElements = function() {
   var shuffledWords = shuffle(words);
 
-  for (var i = 0; i <= 10; i++) {
-    var singleWord = document.createElement('p') // creating 'p' element, calling it singleWord
-    singleWord.innerHTML = "!" + "<" + "#" + shuffledWords[i]; // setting the content of the first word
+  for (var i = 0; i <= 29; i++) {
+    var singleWord = document.createElement('span') // creating 'p' element, calling it singleWord
+    singleWord.innerHTML = "<" + "#" + "*" + shuffledWords[i] + "%"; // setting the content of the first word
     var giantArrayElement = document.querySelector('.giant-array') // selecting .giant-array and storing it in var
     giantArrayElement.appendChild(singleWord); // appending singleWord to giantArrayElement
   }
 }
-
 createAndDisplayWordElements();
-
-// var shuffle = require('shuffle-array'),
-// numTest = [1, 2, 3, 4, 5, 6];
-//
-// shuffle(numTest);
-//
-// console.log(numTest);
-
-
-
-
-
-// var wordsInThisRound = function() {
-//   let sixLetterWords = wordsInThisRound[Math.floor(Math.random() * 10)];
-//   var giantArray = words.push(sixLetterWords[i]);
-// };
-//
-// console.log(wordsInThisRound());
-
-
-
-// Push from var words into giantArray
-
-
-
-
 
 
 ///// ======== ////// GARBAGE CHARACTERS ///// ======== //////
